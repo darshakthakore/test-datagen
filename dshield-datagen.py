8160e4200bb5a0bbf76f4dcadf903ca815d8d854
Darshak Thakore <d.thakore@cablelabs.com>
2016-11-05 15:32:01 -0600

    WIP - intermediate save

--- dshield-datagen.py
+++ dshield-datagen.py
@@ -6,9 +6,12 @@ def main():
     # Get the file name from the command line that contains the raw data
     parser = argparse.ArgumentParser(description='Parse raw DShield data into DIS input format')
     parser.add_argument('--file', nargs='+', required=True, type=argparse.FileType('r'), help='file containing raw DShield data')
+    parser.add_argument('--setSource', help='Set the source for the generated test data')
 
     args = parser.parse_args()
-    infile = args.file[0]
+    print(args)
+    infile = vars(args)
+    print(infile)
 
     # Process each record
     for line in infile.readlines():
@@ -20,11 +23,10 @@ def processRecord(record):
     # The IP address is the first one
     data = record.split()
     ip = data[0]
+    pCount = data[1]
+    destIPs = data[2]
 
 
 if __name__ == '__main__':
     main()
-
-
-
 
